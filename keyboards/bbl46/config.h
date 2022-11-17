5f6b24caf7653295aac20d3bb10f8893776ccdcf
hichino <hay.ichino@gmail.com>
2022-11-17 13:42:30 +0000

    add bbl46

---
+++ keyboards/bbl46/config.h
@@ -0,0 +1,66 @@
+// Copyright 2022 jpwine (@jpwine)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6BF4
+//#define RAW_ENABLE
+//#define DYNAMIC_KEYMAP_ENABLE
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* ws2812 RGB LED */
+
+#ifndef IOS_DEVICE_ENABLE
+  #define RGBLIGHT_LIMIT_VAL 90
+  #define RGBLIGHT_VAL_STEP 17
+#else
+  #define RGBLIGHT_LIMIT_VAL 30
+  #define RGBLIGHT_VAL_STEP 4
+#endif
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+
+#if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
+// USB_MAX_POWER_CONSUMPTION value for naked48 keyboard
+//  120  RGBoff, OLEDoff
+//  120  OLED
+//  330  RGB 6
+//  300  RGB 32
+//  310  OLED & RGB 32
+  #define USB_MAX_POWER_CONSUMPTION 400
+#else
+  // fix iPhone and iPad power adapter issue
+  // iOS device need lessthan 100
+  #define USB_MAX_POWER_CONSUMPTION 100
+#endif
+
+
+// VIA
+//#define DYNAMIC_KEYMAP_LAYER_COUNT 5
---
+++ keyboards/bbl46/rules.mk
@@ -0,0 +1,5 @@
+# This file intentionally left blank
+#
+#VIA_ENABLE = yes
+#RAW_ENABLE = yes
+#DYNAMIC_KEYMAP_ENABLE = yes
